96b7a6b3ffbf7bab5c453d9a2e61d07ab0cc2d12
Roman Bulinski <aboandrus@gmail.com>
2019-05-03 20:23:14 +0200

    Refactor code ( part of code to private function )

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,30 @@
 import java.math.BigInteger;
-import java.util.Arrays;
 
 public class Main {
 
-
     public static BigInteger easyLine(int n) {
+        BigInteger[][] setArr = new BigInteger[n + 1][];
+        setArray(n, setArr);
+        BigInteger tem = new BigInteger("0");
+        BigInteger[] lastArr = setArr[setArr.length - 1];
+        for (BigInteger big : lastArr) {
+            tem = tem.add(big.pow(2));
+        }
+        return tem;
+    }
 
-        BigInteger[][] setArr = new BigInteger[n+1][];
-        int i =0;
-
-        while(i<=n){
-            BigInteger[] tempArr = new BigInteger[i+1];
+    private static void setArray(int n, BigInteger[][] setArr) {
+        int i = 0;
+        while (i <= n) {
+            BigInteger[] tempArr = new BigInteger[i + 1];
             tempArr[0] = new BigInteger("1");
-            for(int k=1; k<tempArr.length-1; k++ ){
-                BigInteger tempBigInt = (setArr[i-1][k-1].add( setArr[i-1][k]));
-                tempArr[k]= tempBigInt;
+            tempArr[tempArr.length - 1] = tempArr[0];
+            for (int k = 1; k < tempArr.length - 1; k++) {
+                tempArr[k] = (setArr[i - 1][k - 1].add(setArr[i - 1][k]));
             }
-            tempArr[tempArr.length-1] = new BigInteger("1");
             setArr[i] = tempArr;
             i++;
         }
-
-        BigInteger tem = new BigInteger("0");
-        BigInteger[] lastArr = setArr[setArr.length-1];
-         for( BigInteger big : lastArr ){
-             tem = tem.add(big.pow(2));
-         }
-
-        return new BigInteger( String.valueOf(tem) );
-    }
-
-
-
-
-    public static void main(String[] args) {
-        System.out.println( easyLine(7));
     }
 
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -8,21 +8,17 @@ public class TestMain {
 
 
     @Test
-    public void test0() {
-        assertEquals( new BigInteger("70"), Main.easyLine(4));
-    }
+    public void test0() { assertEquals( new BigInteger("70"), Main.easyLine(4)); }
 
     @Test
     public void test1() {
         assertEquals( new BigInteger("3432"), Main.easyLine(7));
     }
 
-
     @Test
     public void test2() {
         assertEquals( new BigInteger("10400600"), Main.easyLine(13));
     }
-
 
     @Test
     public void test3() {
@@ -34,12 +30,10 @@ public class TestMain {
         assertEquals( new BigInteger("35345263800"), Main.easyLine(19));
     }
 
-
-
-//    @Test
-//    public void test50() {
-//        assertEquals( new BigInteger("100891344545564193334812497256"), Main.easyLine(50));
-//    }
+    @Test
+    public void test50() {
+        assertEquals( new BigInteger("100891344545564193334812497256"), Main.easyLine(50));
+    }
 
 
 }
